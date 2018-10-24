da790d85f481fb7cdc80c03d7ce8b08e20c3fa75
yurifeng <yuri_feng@outlook.com>
2018-10-24 13:30:24 +0800

    Add files via upload

---
+++ RemarkAttribute.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Reflection;
+
+namespace MyAttribute.Extend
+{
+    /// <summary>
+    /// 是给枚举用  提供一个额外信息
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Enum | AttributeTargets.Field)]
+    public class RemarkAttribute : Attribute
+    {
+        public RemarkAttribute(string remark)
+        {
+            this.Remark = remark;
+        }
+
+        public string Remark { get; private set; }
+    }
+
+    /// <summary>
+    /// 扩展方法
+    /// </summary>
+    /// <param name="enumValue"></param>
+    /// <returns></returns>
+    public static class RemarkExtend
+    {
+
+        public static string GetRemark(this Enum enumValue)
+        {
+            Type type = enumValue.GetType();
+            FieldInfo field = type.GetField(enumValue.ToString());
+            if (field.IsDefined(typeof(RemarkAttribute), true))
+            {
+                RemarkAttribute remarkAttribute =
+                    (RemarkAttribute)field.GetCustomAttribute(typeof(RemarkAttribute));
+                return remarkAttribute.Remark;
+            }
+            else
+            {
+                return enumValue.ToString();
+            }
+        }
+    }
+
+    [Remark("User Status")]
+    public enum UserState
+    {
+        /// <summary>
+        /// 正常
+        /// </summary>
+        [Remark("Normal")]
+        Normal = 0,
+        /// <summary>
+        /// 冻结
+        /// </summary>
+        [Remark("Frozen")]
+        Frozen = 1,
+        /// <summary>
+        /// 删除
+        /// </summary>
+        [Remark("Deleted")]
+        Deleted = 2
+    }
+}
---
+++ ValidateAttribute.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace MyAttribute.Extend
+{
+    //抽象父类
+    public abstract class AbstractValidateAttribute : Attribute
+    {
+        public abstract bool Validate(object oValue);
+    }
+
+    //新建的LongValidateAttribute类继承父类(重写抽象方法)
+    public class LongValidateAttribute : AbstractValidateAttribute
+    {
+        private long _lMin = 0;
+        private long _lMax = 0;
+
+        public LongValidateAttribute(long lMin, long lMax)
+        {
+            this._lMin = lMin;
+            this._lMax = lMax;
+        }
+
+        //范围的检查
+        public override bool Validate(object oValue)
+        {
+            return this._lMin < (long)oValue && (long)oValue < this._lMax;
+        }
+    }
+
+    //新建的RequirdValidateAttribute类继承抽象父类(重写抽象方法)
+    public class RequirdValidateAttribute : AbstractValidateAttribute
+    {
+        //是否为空的检查
+        public override bool Validate(object oValue)
+        {
+            return oValue != null;
+        }
+    }
+
+    /// <summary>
+    /// 数据检验(让特性完成特定功能的类)
+    /// </summary>
+    public class DataValidate
+    {
+        public static bool Validate<T>(T t)
+        {
+            Type type = t.GetType();
+            bool result = true;
+            foreach (var prop in type.GetProperties())
+            {
+                if (prop.IsDefined(typeof(AbstractValidateAttribute), true))
+                {
+                    object item = prop.GetCustomAttributes(typeof(AbstractValidateAttribute), true)[0];
+                    AbstractValidateAttribute attribute = item as AbstractValidateAttribute;
+                    if (!attribute.Validate(prop.GetValue(t)))
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+
+            return result;
+
+        }
+    }
+}
